d747a932e0ce6e682e553f258580c1c768f1dd30
Leonardo <fanger53@gmail.com>
2021-03-11 16:38:26 -0500

    stiling table in index.html

--- scripts/book.js
+++ scripts/book.js
@@ -21,7 +21,7 @@ class UI {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.nPages}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
     `;
     		list.appendChild(row);
 	}
