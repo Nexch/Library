8bac3b707e52090cf501f2ae4f1968f199439682
Leonardo <fanger53@gmail.com>
2021-03-11 21:53:33 -0500

    review linters

--- scripts/book.js
+++ scripts/book.js
@@ -1,99 +1,91 @@
-
+/* eslint-disable max-classes-per-file */
 class Book {
-	constructor(author, title, nPages) {
-		this.author = author;
-		this.title = title;
-		this.nPages = nPages;
-	}
+  constructor(author, title, nPages) {
+    this.author = author;
+    this.title = title;
+    this.nPages = nPages;
+  }
 }
-
 class UI {
-	static displayBooks() {
-		const StoredBooks = Store.getBooks();
-		const books = StoredBooks;
-		books.forEach((book) => UI.addBookToList(book));
-	}
+  static displayBooks() {
+    const StoredBooks = Store.getBooks();
+    const books = StoredBooks;
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
-	static addBookToList(book) {
-		const list = document.querySelector('#book-list');
-		const row = document.createElement('tr');
-		row.innerHTML = `
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.nPages}</td>
       <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
     `;
-    		list.appendChild(row);
-	}
+    list.appendChild(row);
+  }
 
-  	static deleteBook(element) {
-    		if(element.classList.contains('delete')) {
-      			element.parentElement.parentElement.remove();
-    		}
-	}
+  static deleteBook(element) {
+    if (element.classList.contains('delete')) {
+      element.parentElement.parentElement.remove();
+    }
+  }
 
-  	static showAlert(message, className) {
-    		const div = document.createElement('div');
-    		div.className = `alert alert-${className}`;
-    		div.appendChild(document.createTextNode(message));
-    		const container = document.querySelector('.form_box');
-    		const form = document.querySelector('#book-form');
-    		container.insertBefore(div, form);
-    		setTimeout(() => document.querySelector('.alert').remove(), 2000);
-  	}
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.form_box');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+  }
 }
-
 class Store {
-  	static getBooks() {
-    	let books;
-    	if(localStorage.getItem('books') === null) {
-      		books = [];
-    	}
-	else {
-      		books = JSON.parse(localStorage.getItem('books'));
-    	}
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
 
-	return books;
-}
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
-  	static addBook(book) {
-    		const books = Store.getBooks();
-    		books.push(book);
-    		localStorage.setItem('books', JSON.stringify(books));
-  	}
-
-  	static removeBook(nPages) {
-    		const books = Store.getBooks();
-
-    		books.forEach((book, index) => {
-      			if(book.nPages === nPages) {
-        			books.splice(index, 1);
-      			}
-    		});
-
-    		localStorage.setItem('books', JSON.stringify(books));
-  	}
+  static removeBook(nPages) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.nPages === nPages) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-  	e.preventDefault();
-  	const title = document.querySelector('#title').value;
-  	const author = document.querySelector('#author').value;
-  	const nPages = document.querySelector('#nPages').value;
+  e.preventDefault();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const nPages = document.querySelector('#nPages').value;
 
-	if (title === '' || author === '' || nPages === '') {
-		UI.showAlert('Please fill in all fields', 'danger');
-  		}
-  	else {
-    		const book = new Book(title, author, nPages);
-		UI.addBookToList(book);
-		Store.addBook(book)
-    		UI.showAlert('Book Added', 'success');
-	}
+  if (title === '' || author === '' || nPages === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    const book = new Book(title, author, nPages);
+    UI.addBookToList(book);
+    Store.addBook(book);
+    UI.showAlert('Book Added', 'success');
+  }
 });
-
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  	UI.deleteBook(e.target);
-	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  UI.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
